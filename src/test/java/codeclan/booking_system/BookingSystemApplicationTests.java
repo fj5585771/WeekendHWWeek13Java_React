173618749de4eb3e40f0833c8f9a823f87c0dd4e
Nikolas Theodoropoulos <nikolastheodoropoulos@Nikolass-MacBook-Air.local>
2021-02-11 13:19:39 +0200

    first commit

---
+++ src/test/java/codeclan/booking_system/BookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package codeclan.booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
